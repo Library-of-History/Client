ec91a128a7bfdc57a7a73403e9dcee2b64b06af1
ryoon114 <kilhoon114@gmail.com>
2025-08-03 15:09:34 +0900

    이동방식 teleportetion

---
+++ Assets/Hangilhoon/Script/Scence_change.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scence_change : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        SceneManager.LoadScene("Loby");
+    }
+
+   
+}
